f91a7786799985e971f54e4219c2e2c093f9077c
Robakuuu <daniel.robaszkiewicz@wp.pl>
2021-03-10 21:01:17 +0100

    Fast3DVisual 0.1.1

--- DrawScene.cpp
+++ DrawScene.cpp
@@ -1,12 +1,26 @@
 #include "DrawScene.h"
 #include "Camera.h"
 #include "ObjFile.h"
+#include <windows.h>
+#include <shellapi.h>
+#include <iostream>
 
 
+std::string global_PathsToObjects="";
+
+void DropCallback(GLFWwindow* window, int count, const char** paths)
+{
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        std::cout << "- " << paths[i] << std::endl;
+    }
+    global_PathsToObjects = paths[0];
+}
 
 Scene::Scene()
 {
- 
+    
     GLFWwindow* window;
     bool bglfwInitOK = true;
     bool bWindowOpened = true;
@@ -26,23 +40,49 @@ Scene::Scene()
     if (bWindowOpened && bglfwInitOK == true)
     {
         
+        bool bNewObject = false;
+        bool bFirstStart = true;
+        std::string oLastPath = "";
+
         glfwMakeContextCurrent(window);
         glewInit();
-        PrepareBuffer();
+        glfwSetDropCallback(window, DropCallback);
+      
+
         Camera oCamera(VERTEX_SHADER, FRAGMENT_SHADER);
 
+        
+        glGenBuffers(1, &m_GLIntVertexBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, m_GLIntVertexBuffer);
+      // glGenBuffers(1, &m_GLIntUvBuffer);
+      //  glBindBuffer(GL_ARRAY_BUFFER, m_GLIntUvBuffer);
         while (!glfwWindowShouldClose(window))
         {
-			
-			
-			
-			glUniformMatrix4fv(oCamera.GetMatrixID(), 1, GL_FALSE, &oCamera.GetMvp()[0][0]);
+            if (global_PathsToObjects != oLastPath)
+            {
+               
+                    bNewObject = true;
+                
+            }
+            if (bNewObject == true)
+            {
+                PrepareBuffer(global_PathsToObjects.c_str());
+                oLastPath = global_PathsToObjects;
+                bNewObject = false;
+                bFirstStart = false;
+            }
+           
+            
+            if(bNewObject==false)
+            {
+                glUniformMatrix4fv(oCamera.GetMatrixID(), 1, GL_FALSE, &oCamera.GetMvp()[0][0]);
 
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			glUseProgram(oCamera.GetProgramID());
-            DrawScene();
-            glfwSwapBuffers(window);
-            glfwPollEvents();
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                glUseProgram(oCamera.GetProgramID());
+                DrawScene();
+                glfwSwapBuffers(window);
+                glfwPollEvents();
+            }
 			
         }
     }
@@ -51,27 +91,22 @@ Scene::Scene()
     glfwTerminate();
 }
 
-void Scene::PrepareBuffer()
+void Scene::PrepareBuffer(const char* pathToObject)
 {
 
-    ObjFile oObjObject("obj1.obj");
+    ObjFile oObjObject(pathToObject);
     m_vec3Vertices = oObjObject.GetVertices();
     m_vec2Uv = oObjObject.GetUvs();
 
-
-    glGenBuffers(1, &m_GLIntVertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, m_GLIntVertexBuffer);
+   
+  
     glBufferData(GL_ARRAY_BUFFER, m_vec3Vertices.size() * sizeof(glm::vec3), &m_vec3Vertices[0], GL_STATIC_DRAW);
-
-
-    glGenBuffers(1, &m_GLIntUvBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, m_GLIntUvBuffer);
-    glBufferData(GL_ARRAY_BUFFER, m_vec2Uv.size() * sizeof(glm::vec2), &m_vec2Uv[0], GL_STATIC_DRAW);
+    //glBufferData(GL_ARRAY_BUFFER, m_vec2Uv.size() * sizeof(glm::vec2), &m_vec2Uv[0], GL_STATIC_DRAW);
 }
 void Scene::DrawScene()
 {
 	glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, m_GLIntVertexBuffer);
+   // glBindBuffer(GL_ARRAY_BUFFER, m_GLIntVertexBuffer);
 	glVertexAttribPointer(
 		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
 		3,                  // size
--- DrawScene.h
+++ DrawScene.h
@@ -1,8 +1,11 @@
 #pragma once
+#define GLFW_EXPOSE_NATIVE_WIN32
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/glm.hpp>
+
 #include <stdio.h>
 #include <string>
 #include <fstream>
@@ -15,7 +18,7 @@ class Scene
 {
 public:
 	Scene();
-	void PrepareBuffer();
+	void PrepareBuffer(const char* pathToObject);
 	void DrawScene();
 	GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path);
 
@@ -23,6 +26,7 @@ private:
 	GLuint VertexArrayID;
 	GLuint m_GLIntVertexBuffer;
 	GLuint m_GLIntUvBuffer;
+	const char* m_pcharPathToObject;
 	
 	std::vector< glm::vec3 > m_vec3Vertices;
 	std::vector< glm::vec2 > m_vec2Uv;
